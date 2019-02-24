23a2a3137d4689d311303486a1b9499964571ebd
BrutalTostitos <josh.wynn27@gmail.com>
2019-02-23 21:07:52 -0500

    Setting up basics
    base gamecontroller
    base playercontroller
    a sample scene

---
+++ Assets/CoreCode/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController
+{
+
+    /// <summary>
+    /// The save game name.
+    /// </summary>
+    private static string mFileName;
+
+    /// <summary>
+    /// The instance of the GameController.
+    /// </summary>
+    private static GameController mInstance;
+
+    private bool isPaused;
+
+
+    private GameController()
+    {
+        //init stuff
+    }
+
+    //Singleton
+    public static GameController GetInstance()
+    {
+        //TODO: make this throw an exception to 'encourage' use of Init()
+        if (mInstance == null)
+        {
+            mInstance = new GameController();
+        }
+
+        return mInstance;
+    }
+
+    /// <summary>
+    /// Initializes the GameController
+    /// </summary>
+    public static void Init()
+    {
+        mInstance = new GameController();
+    }
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+    }
+
+    public bool GetPaused()
+    {
+        return isPaused;
+    }
+
+
+}
---
+++ Assets/CoreCode/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    //Public variables
+    public bool mFacingRight = true;
+    public float mSpeed;
+    public float jumpForce;
+    public Vector2 moveInput;   // = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+    //Will be used for gravity
+    public bool isGrounded = true;
+
+
+    //private variables
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    //manages anything dealing with physics
+    private void FixedUpdate()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        rb.velocity = moveInput * mSpeed;
+        
+        if (mFacingRight == false && moveInput.x > 0)
+        {
+            Flip();
+        }
+        if (mFacingRight == true && moveInput.x < 0)
+        {
+            Flip();
+        }
+
+    }
+
+
+
+
+
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        mFacingRight = !mFacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
